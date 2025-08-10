823eee0051af3927e4d81afad07c25982adf66ca
Sonu Kushwaha <sonukushwaha51@gmail.com>
2025-08-10 18:26:14 +0530

    First commit for schema

---
+++ com.handson.graphql.schema/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.handson.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
